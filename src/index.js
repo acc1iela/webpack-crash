912c6e6dd232c880af64ce70d2d145edaee1f666
Accio26 <acc1iela@gmail.com>
2020-01-22 17:19:17 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import _ from 'lodash';
 
+import {
+  NAME,
+  NiJou
+} from './utilities'
+
+import * as utilities from './utilities'
+
+import {
+  NAME as NAME_OF_HAM
+} from './utilities'
+
+import Lion from './utilities'
+
+console.log(NiJou(3));
+console.log(NAME);
+console.log(utilities.NAME);
+console.log(NAME_OF_HAM);
+console.log(Lion.say());
+
+
 function component() {
   const element = document.createElement('div');
   const array = ['Hello', 'webpack', '!!'];
